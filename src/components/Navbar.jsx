c452cccc75b6df5aeecbe040cbdc69215770cc9f
thomasAmorgan <thomasmorgan233@gmail.com>
2018-10-06 15:46:53 -0400

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <NavLink to="/">Home</NavLink>
+      </div>
+    );
+  }
+}
